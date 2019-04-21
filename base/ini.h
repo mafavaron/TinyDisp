257bdf06c8bf2318158fa861b4a215c3209271e7
Mauri Favaron <mafavaron@mac.com>
2019-04-21 09:09:26 +0200

    Create ini.h

---
+++ base/ini.h
@@ -0,0 +1,26 @@
+//
+// Created by Maurizi Favaron on 2019-04-20.
+//
+
+#ifndef TINYPART_INI_H
+#define TINYPART_INI_H
+
+#include <string>
+#include <vector>
+#include <map>
+
+class ini {
+private:
+    std::string                         sIniFileName;
+    std::vector<std::string>            svSection;
+    std::vector<std::string>            svKey;
+    std::vector<std::string>            svValue;
+    std::map<std::string,std::string>   mValues;
+public:
+    ini(void);
+    ini(const std::string sFileName);
+    virtual ~ini();
+};
+
+
+#endif //TINYPART_INI_H
