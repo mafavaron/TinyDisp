cea3ab24d4b534b2d040b5d5eed2431ad7343e77
Mauri Favaron <mafavaron@mac.com>
2019-07-27 18:52:32 +0200

    Create meteo.f90

---
+++ src/meteo.f90
@@ -0,0 +1,9 @@
+! meteo.f90 - Pre-processor, reading data in ALAMO form and converting them to full profiled form
+!             suitable for TinyPart use.
+!
+! Written by: Mauri Favaron
+!
+
+program Meteo
+
+end program Meteo
