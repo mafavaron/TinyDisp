da9db8638b760c096d401d94b67b0329f9fe9092
Mauri Favaron <mafavaron@mac.com>
2019-07-28 20:46:44 +0200

    Update metpre.f90
    Config and meteo read added

--- src/metpre.f90
+++ src/metpre.f90
@@ -1,9 +1,42 @@
-! meteo.f90 - Pre-processor, reading data in ALAMO form and converting them to full profiled form
-!             suitable for TinyPart use.
+! metpre.f90 - Pre-processor, reading data in ALAMO form and converting them to full profiled form
+!              suitable for TinyPart use.
 !
 ! Written by: Mauri Favaron
 !
 
-program Meteo
+program Meteo_Preprocessor
 
-end program Meteo
+	use Processing
+	
+	implicit none
+	
+	! Locals
+	character(len=256)	:: sConfigFile
+	integer				:: iRetCode
+	type(Config)		:: tConfig
+	
+	! Get parameters
+	if(command_argument_count() /= 1) then
+		print *, "metpre - Meteorological preparer for TinyDisp"
+		print *
+		print *, "Usage:"
+		print *
+		print *, "  ./metpre <Config_File_Name>"
+		print *
+		print *, "Copyright 2019 by Servizi Territorio srl"
+		print *, "This is open-source software, covered by MIT license."
+		print *
+		print *, "Written by: Mauri Favaron"
+		print *
+		stop 1
+	end if
+	call get_command_argument(1, sConfigFile)
+	
+	! Process data
+	iRetCode = tConfig % read(10, 11, sConfigFile)
+	if(iRetCode /= 0) then
+		print *, "metpre:: error: Input file not opened - Return code = ", iRetCode
+		stop
+	end if
+
+end program Meteo_Preprocessor
