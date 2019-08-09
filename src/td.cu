91566834849ab3d563f9109ef17db95f855cd56b
Mauri Favaron <mafavaron@mac.com>
2019-08-09 18:49:02 +0200

    Update td.cu
    First reference to actual thrust data added (and proven compile-OK)

--- src/td.cu
+++ src/td.cu
@@ -8,10 +8,15 @@
 #include <stdio.h>
 #include <math.h>
 
+#define N (1<<20)
+
 int main(int argc, char** argv) {
 
 	curandGenerator_t gen;
 	curandCreateGenerator(&gen, CURAND_RNG_PSEUDO_DEFAULT);
 	curandSetPseudoRandomGeneratorSeed(gen, NULL);
+
+	thrust::device_vector<float> dvec_x(N);
+	thrust::device_vector<float> dvec_y(N);
 
 }
