f109722a1c772ad96ee36e8b072b37d4c92e6b11
Mauri Favaron <mafavaron@mac.com>
2019-08-17 09:42:38 +0200

    Update meteo.h
    Translation from Fortran in progress

--- src/meteo.h
+++ src/meteo.h
@@ -7,32 +7,32 @@
 
 class Meteo {
 private:
-  ! Time stamp
-  real(8)								:: rEpoch	! Time stamp of current profile set
-  ! Primitive profiles
-  real(8), dimension(:), allocatable	:: z		! Levels' height above ground (m)
-  real(8), dimension(:), allocatable	:: u		! U components (m/s)
-  real(8), dimension(:), allocatable	:: v		! V components (m/s)
-  real(8), dimension(:), allocatable	:: T		! Temperatures (K)
-  real(8), dimension(:), allocatable	:: su2		! var(U) values (m2/s2)
-  real(8), dimension(:), allocatable	:: sv2		! var(V) values (m2/s2)
-  real(8), dimension(:), allocatable	:: sw2		! var(W) values (m2/s2)
-  real(8), dimension(:), allocatable	:: dsw2		! d var(W) / dz (m/s2)
-  real(8), dimension(:), allocatable	:: eps		! TKE dissipation rate
-  real(8), dimension(:), allocatable	:: alfa		! Langevin equation coefficient
-  real(8), dimension(:), allocatable	:: beta		! Langevin equation coefficient
-  real(8), dimension(:), allocatable	:: gamma	! Langevin equation coefficient
-  real(8), dimension(:), allocatable	:: delta	! Langevin equation coefficient
-  real(8), dimension(:), allocatable	:: alfa_u	! Langevin equation coefficient
-  real(8), dimension(:), allocatable	:: alfa_v	! Langevin equation coefficient
-  real(8), dimension(:), allocatable	:: deltau	! Langevin equation coefficient
-  real(8), dimension(:), allocatable	:: deltav	! Langevin equation coefficient
-  real(8), dimension(:), allocatable	:: deltat	! Langevin equation coefficient
-  ! Convenience derived values
-  real(8), dimension(:), allocatable	:: Au		! exp(alfa_u*dt)
-  real(8), dimension(:), allocatable	:: Av		! exp(alfa_v*dt)
-  real(8), dimension(:), allocatable	:: A		! exp(alfa*dt)
-  real(8), dimension(:), allocatable	:: B		! exp(beta*dt)
+  // Time stamp
+  double				 rEpoch	// Time stamp of current profile set
+  // Primitive profiles
+  vector<double> z		  // Levels' height above ground (m)
+  vector<double> u		  // U components (m/s)
+  vector<double> v		  // V components (m/s)
+  vector<double> T		  // Temperatures (K)
+  vector<double> su2		// var(U) values (m2/s2)
+  vector<double> sv2		// var(V) values (m2/s2)
+  vector<double> sw2		// var(W) values (m2/s2)
+  vector<double> dsw2		// d var(W) / dz (m/s2)
+  vector<double> eps		// TKE dissipation rate
+  vector<double> alfa		// Langevin equation coefficient
+  vector<double> beta		// Langevin equation coefficient
+  vector<double> gamma	// Langevin equation coefficient
+  vector<double> delta	// Langevin equation coefficient
+  vector<double> alfa_u	// Langevin equation coefficient
+  vector<double> alfa_v	// Langevin equation coefficient
+  vector<double> deltau	// Langevin equation coefficient
+  vector<double> deltav	// Langevin equation coefficient
+  vector<double> deltat	// Langevin equation coefficient
+  // Convenience derived values
+  vector<double> Au		 // exp(alfa_u*dt)
+  vector<double> Av		 // exp(alfa_v*dt)
+  vector<double> A		 // exp(alfa*dt)
+  vector<double> B		 // exp(beta*dt)
 public:
   procedure	:: clean      => metpClean
   procedure	:: alloc      => metpAlloc
