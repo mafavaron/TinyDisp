083868f60b3e6952307e7746b79f3ce49fd70fe3
Mauri Favaron <mafavaron@mac.com>
2019-08-18 17:24:51 +0200

    Update meteodata.cpp
    Space reservation made for all member vectors

--- src/meteodata.cpp
+++ src/meteodata.cpp
@@ -2,6 +2,28 @@
 
 MeteoData::MeteoData(int n) {
   this->z.reserve(n);
+  this->u.reserve(n);
+  this->v.reserve(n);
+  this->w.reserve(n);
+  this->T.reserve(n);
+  this->su2.reserve(n);
+  this->sv2.reserve(n);
+  this->sw2.reserve(n);
+  this->dsw2.reserve(n);
+  this->eps.reserve(n);
+  this->alfa.reserve(n);
+  this->beta.reserve(n);
+  this->gamma.reserve(n);
+  this->delta.reserve(n);
+  this->alfa_u.reserve(n);
+  this->alfa_v.reserve(n);
+  this->deltau.reserve(n);
+  this->deltav.reserve(n);
+  this->deltat.reserve(n);
+  this->Au.reserve(n);
+  this->Av.reserve(n);
+  this->A.reserve(n);
+  this->B.reserve(n);
 };
 
 
