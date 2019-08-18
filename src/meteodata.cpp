7a8086156cfc9b34d2d731a6c07c1a2ffbea9ca0
Mauri Favaron <mafavaron@mac.com>
2019-08-18 18:00:55 +0200

    Update meteodata.cpp
    One record read statements added

--- src/meteodata.cpp
+++ src/meteodata.cpp
@@ -37,8 +37,54 @@ int MeteoData::Get(std::ifstream& cfg, const int n) {
 
   // Get oen record< if present
   if(cfg.is_open()) {
+    this->z.clear();
+    this->u.clear();
+    this->v.clear();
+    this->w.clear();
+    this->T.clear();
+    this->su2.clear();
+    this->sv2.clear();
+    this->sw2.clear();
+    this->dsw2.clear();
+    this->eps.clear();
+    this->alfa.clear();
+    this->beta.clear();
+    this->gamma.clear();
+    this->delta.clear();
+    this->alfa_u.clear();
+    this->alfa_v.clear();
+    this->deltau.clear();
+    this->deltav.clear();
+    this->deltat.clear();
+    this->Au.clear();
+    this->Av.clear();
+    this->A.clear();
+    this->B.clear();
+    cfg.read((char*)&this->rEpoch, sizeof(double));
     for(int i=0; i<n; i++) {
-      cfg.read((char*)&this->iDebugLevel, sizeof(int));
+      cfg.read((char*)&this->z[i], sizeof(double));
+      cfg.read((char*)&this->u[i], sizeof(double));
+      cfg.read((char*)&this->v[i], sizeof(double));
+      this->w[i] = 0.0;
+      cfg.read((char*)&this->T[i], sizeof(double));
+      cfg.read((char*)&this->su2[i], sizeof(double));
+      cfg.read((char*)&this->sv2[i], sizeof(double));
+      cfg.read((char*)&this->sw2[i], sizeof(double));
+      cfg.read((char*)&this->dsw2[i], sizeof(double));
+      cfg.read((char*)&this->eps[i], sizeof(double));
+      cfg.read((char*)&this->alfa[i], sizeof(double));
+      cfg.read((char*)&this->beta[i], sizeof(double));
+      cfg.read((char*)&this->gamma[i], sizeof(double));
+      cfg.read((char*)&this->delta[i], sizeof(double));
+      cfg.read((char*)&this->alfa_u[i], sizeof(double));
+      cfg.read((char*)&this->alfa_v[i], sizeof(double));
+      cfg.read((char*)&this->deltau[i], sizeof(double));
+      cfg.read((char*)&this->deltav[i], sizeof(double));
+      cfg.read((char*)&this->deltat[i], sizeof(double));
+      cfg.read((char*)&this->Au[i], sizeof(double));
+      cfg.read((char*)&this->Av[i], sizeof(double));
+      cfg.read((char*)&this->A[i], sizeof(double));
+      cfg.read((char*)&this->B[i], sizeof(double));
     }
   }
 
