178f56dec9151c0cd1ae0c22177d1416c4fbad54
Mauri Favaron <mafavaron@mac.com>
2019-08-18 16:04:24 +0200

    Update cfg.h
    Return type for Get member function added

--- src/cfg.h
+++ src/cfg.h
@@ -1,57 +1,42 @@
-#ifndef __CFG__
+#ifndef __METEO__
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
-class Cfg {
+class Meteo {
 private:
-  // Internal status
-  int         iState;
-  // General
-  int         iDebugLevel;
-  std::string sDiaFile;
-  int         iFrameInterval;
-  std::string sFramePath;
-  int         iExecMode;
-  // Timing
-  int         iAvgTime;
-  int         iNumStep;
-  int         iNumPart;
-  int         iMaxAge;
-  // Emission
-  std::string sStatic;
-  std::string sDynamic;
-  // Meteo
-  std::string sMetInpFile;
-  std::string sMetOutFile;
-  std::string sMetDiaFile;
-  double      rHeight;
-  double      rZ0;
-  double      rZr;
-  double      rZt;
-  double      rGamma;
-  int         iHemisphere;
-  // Output
-  std::string sConcFile;
-  double      rX0;
-  double      rY0;
-  int         iNx;
-  int         iNy;
-  double      rDx;
-  double      rDy;
-  int         iNz;
-  double      rDz;
-  double      rFactor;
+  // Time stamp
+  double				 rEpoch	// Time stamp of current profile set
+  // Primitive profiles
+  vector<double> z		  // Levels' height above ground (m)
+  vector<double> u		  // U components (m/s)
+  vector<double> v		  // V components (m/s)
+  vector<double> T		  // Temperatures (K)
+  vector<double> su2		// var(U) values (m2/s2)
+  vector<double> sv2		// var(V) values (m2/s2)
+  vector<double> sw2		// var(W) values (m2/s2)
+  vector<double> dsw2		// d var(W) / dz (m/s2)
+  vector<double> eps		// TKE dissipation rate
+  vector<double> alfa		// Langevin equation coefficient
+  vector<double> beta		// Langevin equation coefficient
+  vector<double> gamma	// Langevin equation coefficient
+  vector<double> delta	// Langevin equation coefficient
+  vector<double> alfa_u	// Langevin equation coefficient
+  vector<double> alfa_v	// Langevin equation coefficient
+  vector<double> deltau	// Langevin equation coefficient
+  vector<double> deltav	// Langevin equation coefficient
+  vector<double> deltat	// Langevin equation coefficient
+  // Convenience derived values
+  vector<double> Au		 // exp(alfa_u*dt)
+  vector<double> Av		 // exp(alfa_v*dt)
+  vector<double> A		 // exp(alfa*dt)
+  vector<double> B		 // exp(beta*dt)
 public:
-  // Constructors, destructor
-  Cfg();
-  Cfg(std::ifstream& cfg);
-  ~Cfg();
-  // Parameter check
-  int Validate(void);
-  // Access to components
-  int GetState(void);
+  Meteo(int n);
+  ~Meteo();
+  int Get(std::ifstream& cfg);
 };
 
 #endif
