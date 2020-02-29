eee8da2e7a783d174814c8b13abcfb801377f20f
Patrizia M. Favaron <mafavaron@mac.com>
2020-02-29 19:04:04 +0100

    Update Config.f90
    Meteo file read added

--- src/Config.f90
+++ src/Config.f90
@@ -4,13 +4,23 @@
 !
 module Config
 
+    use Calendar
+
     implicit none
     
     type ConfigType
+        ! -1- General
         real                :: rTimeStep        ! In seconds, strictly positive
         real                :: rEdgeLength      ! In metres
         character(len=256)  :: sMeteoFile       ! The desired one
         logical             :: lIsValid         ! Use data type only if .TRUE.
+        ! -1- Meteorology
+        integer, dimension(:), allocatable  :: ivTimeStamp
+        real, dimension(:), allocatable     :: rvU
+        real, dimension(:), allocatable     :: rvV
+        real, dimension(:), allocatable     :: rvStdDevU
+        real, dimension(:), allocatable     :: rvStdDevV
+        real, dimension(:), allocatable     :: rvCovUV
     contains
         procedure           :: gather           ! Gets configuration from a NAMELIST file
     end type ConfigType
@@ -26,15 +36,27 @@ contains
         integer                         :: iRetCode
     
         ! Locals
+        ! -1- Just variables
+        integer             :: iErrCode
         real                :: rTimeStep
         real                :: rEdgeLength
+        character(len=256)  :: sBuffer
         character(len=256)  :: sMeteoFile
-        integer             :: iErrCode
         logical             :: lIsFile
+        integer             :: iNumLines
+        integer             :: iLine
         namelist /configuration/ &
             rTimeStep, &
             rEdgeLength, &
             sMeteoFile
+        ! -1- From meteo file
+        integer             :: iCurTime
+        real                :: rU
+        real                :: rV
+        real                :: rStdDevU
+        real                :: rStdDevV
+        real                :: rCovUV
+        integer             :: iYear, iMonth, iDay, iHour, iMinute, iSecond
         
         ! Assume success (will falsify on failure)
         iRetCode = 0
@@ -53,7 +75,7 @@ contains
         end if
         close(iLUN)
         
-        ! Validate configuration
+        ! Validate general configuration
         this % lIsValid = .false.
         if(rTimeStep <= 0.) then
             print *, "cfg> Invalid time step: value is zero or negative, should be positive"
@@ -61,15 +83,70 @@ contains
         end if
         if(rEdgeLength <= 0.) then
             print *, "cfg> Invalid edge length: value is zero or negative, should be positive"
-            iRetCode = 3
+            iRetCode = 4
         end if
         inquire(file = sMeteoFile, exist = lIsFile)
         if(.not.lIsFile) then
             print *, "cfg> Invalid met file: file does not exist"
-            iRetCode = 3
+            iRetCode = 5
         end if
+        if(iRetCode > 0) return
         
-        ! Form configuration, and declare it valid
+        ! Get meteo data
+        open(iLUN, file=sMeteoFile, status='old', action='read', iostat=iErrCode)
+        if(iErrCode /= 0) then
+            iRetCode = 6
+            return
+        end if
+        read(iLUN, "(a)", iostat=iErrCode) sBuffer
+        if(iErrCode /= 0) then
+            iRetCode = 7
+            close(iLUN)
+            return
+        end if
+        iNumLines = 0
+        do
+            read(iLUN, "(a)", iostat=iErrCode) sBuffer
+            if(iErrCode > 0) exit
+            read(sBuffer(1:19), "(i4,5(1x,i2))", iostat=iErrCode) iYear, iMonth, iDay, iHour, iMinute, iSecond
+            if(iErrCode /= 0) exit
+            read(sBuffer(20:), *, iostat=iErrCode) rU, rV, rStdDevU, rStdDevV, rCovUV
+            if(iErrCode /= 0) exit
+            iNumLines = iNumLines + 1
+        end do
+        if(iNumLines <= 0) then
+            iRetCode = 8
+            close(iLUN)
+            return
+        end if
+        rewind(iLUN)
+        if(allocated(this % ivTimeStamp)) deallocate(this % ivTimeStamp)
+        allocate(this % ivTimeStamp(iNumLines))
+        if(allocated(this % rvU))         deallocate(this % rvU)
+        allocate(this % rvU(iNumLines))
+        if(allocated(this % rvV))         deallocate(this % rvV)
+        allocate(this % rvV(iNumLines))
+        if(allocated(this % rvStdDevU))   deallocate(this % rvStdDevU)
+        allocate(this % rvStdDevU(iNumLines))
+        if(allocated(this % rvStdDevV))   deallocate(this % rvStdDevV)
+        allocate(this % rvStdDevV(iNumLines))
+        if(allocated(this % rvCovUV))     deallocate(this % rvCovUV)
+        allocate(this % rvCovUV(iNumLines))
+        read(iLUN, "(a)") sBuffer
+        do iLine = 1, iNumLines
+            read(iLUN, "(a)") sBuffer
+            read(sBuffer(1:19), "(i4,5(1x,i2))") iYear, iMonth, iDay, iHour, iMinute, iSecond
+            call PackTime(this % ivTimeStamp(iLine), iYear, iMonth, iDay, iHour, iMinute, iSecond)
+            read(sBuffer(20:), *) &
+                this % rvU(iLine), &
+                this % rvV(iLine), &
+                this % rvStdDevU(iLine), &
+                this % rvStdDevV(iLine), &
+                this % rvCovUV(iLine)
+        end do
+        close(iLUN)
+        
+        ! Form all what remains of configuration, and declare it valid
         this % rTimeStep   = rTimeStep
         this % rEdgeLength = rEdgeLength
         this % sMeteoFile  = sMeteoFile
