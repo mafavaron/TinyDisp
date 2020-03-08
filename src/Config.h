374227723860dcf6b0ab11ba9297172c79e0c7fa
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-08 18:48:54 +0100

    Update Config.h
    Meteo data vector added

--- src/Config.h
+++ src/Config.h
@@ -1,13 +1,24 @@
 #pragma once
+
+#include <vector>
+
 class Config
 {
 private:
+	// General part
 	bool		lIsValid;
 	int			iTimeStep;
 	float		rEdgeLength;
 	int			iPartsPerStep;
 	int			iStepsSurvival;
 	std::string	sMeteoFile;
+	// Meteorology
+	std::vector<int>	ivTimeStamp;
+	std::vector<float>	rvU;
+	std::vector<float>	rvV;
+	std::vector<float>	rvStdDevU;
+	std::vector<float>	rvStdDevV;
+	std::vector<float>	rvCovUV;
 public:
 	Config(const std::string sMeteoFile);
 	virtual ~Config();
