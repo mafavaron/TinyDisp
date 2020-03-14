386684a493d1bfb687e5d91597836cb1b62c511b
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-14 13:48:58 +0100

    Update FileMgr.cpp
    File list population completed, up to tests

--- src/FileMgr.cpp
+++ src/FileMgr.cpp
@@ -1,15 +1,31 @@
 #include "FileMgr.h"
 #include <filesystem>
+#include <regex>
+
+namespace fs = std::filesystem;
 
 FileMgr::FileMgr(const std::string sPath, const std::string sSearchMask) {
+
+	// Create regular expression from the search pattern
+	std::regex reMatch(sSearchMask, std::regex_constants::nosubs);
+	std::smatch match;
 
 	// Clean out file list, first of all
 	this->svFileName.clear();
 
 	// Scan the path indicated looking for files satisfying the search mask
-	std::filesystem::path tDataPath(sPath);
-	for (const auto& entry : std::filesystem::directory_iterator(sPath)) {
-
+	fs::path tDataPath(sPath);
+	for (const auto& entry : fs::directory_iterator(sPath)) {
+		const auto sFileName = entry.path().filename().string();
+		if (entry.is_directory()) {
+			// Ignore it
+		}
+		else {
+			// Check conformance to file search mask
+			if (std::regex_search(sFileName, match, reMatch)) {
+				this->svFileName.push_back(sFileName);
+			}
+		}
 	}
 
 	// Store path and search mask
