669980231ef4e99adfce5f635e4bfa39bcb84dab
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-14 20:36:51 +0100

    Update FileMgr.h
    Function added to get .visit file name

--- src/FileMgr.h
+++ src/FileMgr.h
@@ -16,5 +16,6 @@ public:
 	bool FileMgr::MapFiles(const std::string sPath, const std::string sSearchMask);
 	bool FileMgr::CreateAndCleanPath(void);
 	std::string FileMgr::GetInnermostDirectory(void);
+	std::string FileMgr::GetVisItName(void);
 };
 
