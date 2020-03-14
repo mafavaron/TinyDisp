d1d031b8a7544878cd935241adc0446b940af585
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-14 17:26:02 +0100

    Update FileMgr.h
    Empty constructor added (for normal use!)

--- src/FileMgr.h
+++ src/FileMgr.h
@@ -11,6 +11,7 @@ private:
 	std::string					sSearchMask;
 	std::vector<std::string>	svFileName;
 public:
+	FileMgr(void);
 	FileMgr(const std::string sPath, const std::string sSearchMask);
 	virtual ~FileMgr(void);
 	bool FileMgr::CreateAndCleanPath(void);
