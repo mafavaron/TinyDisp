c16b084f60066f2a8a2ee5c1158f6b4855f75dea
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-14 11:55:26 +0100

    Update FileMgr.cpp
    In progress

--- src/FileMgr.cpp
+++ src/FileMgr.cpp
@@ -1,6 +1,21 @@
 #include "FileMgr.h"
+#include <filesystem>
 
 FileMgr::FileMgr(const std::string sPath, const std::string sSearchMask) {
+
+	// Clean out file list, first of all
+	this->svFileName.clear();
+
+	// Scan the path indicated looking for files satisfying the search mask
+	std::filesystem::path tDataPath(sPath);
+	for (const auto& entry : std::filesystem::directory_iterator(sPath)) {
+
+	}
+
+	// Store path and search mask
+	this->sBasePath   = sPath;
+	this->sSearchMask = sSearchMask;
+
 };
 
 FileMgr::~FileMgr(void) {
