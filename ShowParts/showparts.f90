f8bb3f248073370f09731ef3f74c69f20469c316
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-21 14:48:29 +0100

    Create showparts.f90
    Initial version of "good-old-style Fortran 2008 visualizer"... :)

---
+++ ShowParts/showparts.f90
@@ -0,0 +1,38 @@
+program showparts
+
+    use config
+    use data_file
+
+    implicit none
+    
+    ! Locals
+    integer             :: iRetCode
+    character(len=256)  :: sConfigFile
+    character(len=256)  :: sInputFile
+    character(len=256)  :: sMovieFile
+    type(ConfigType)    :: tCfg
+    
+    ! Check input parameters
+    if(command_argument_count() /= 0) then
+        print *, "showparts - Movie producer for TinyDisp outputs"
+        print *
+        print *, "Usage:"
+        print *
+        print *, "  showparts <ConfigFile> <MovieFile>"
+        print *
+        print *, "Copyright 2020 by Servizi Territorio srl"
+        print *, "This is open-source software, covered by the MIT license"
+        print *
+        stop
+    end if
+    call get_command_argument(1, sConfigFile)
+    call get_command_argument(2, sMovieFile)
+
+    ! Get configuration
+    iRetCode = tCfg % Read(10, sConfigFile)
+    if(iRetCode /= 0) then
+        print *, "Error: Configuration not read"
+        stop
+    end if
+    
+end program showparts
