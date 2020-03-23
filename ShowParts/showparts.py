6953d6078930e7b62d02fc89ba68cfb845cd61d1
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-23 10:05:38 +0100

    Update showparts.py
    In progress

--- ShowParts/showparts.py
+++ ShowParts/showparts.py
@@ -2,8 +2,38 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import configparser
 import os
 import sys
+
+# System state
+global fParticles
+global sDataFile
+global rEdgeLength
+
+
+def init():
+
+    # Assume success (will falsify on failure)
+    iRetCode = 0
+
+    # Start reading the particle binary file
+    try:
+        fParticles = open(sDataFile, "rb")
+    except:
+        iRetCode = 1
+        return iRetCode
+    try:
+        iMaxPart = np.fromfile(fParticles, dtype=np.int32)
+    except:
+        iRetCode = 2
+        fParticles.close()
+        return iRetCode
+
+    print(iMaxPart)
+
+    return iRetCode
 
 if __name__ == "__main__":
 
@@ -21,3 +51,21 @@ if __name__ == "__main__":
         sys.exit(1)
     sCfgFile = sys.argv[1]
     sMp4File = sys.argv[2]
+
+    # Get configuration data
+    cfg = configparser.ConfigParser()
+    try:
+        cfg.read(sCfgFile)
+    except:
+        print("Error: Configuration file not read")
+        sys.exit(2)
+    try:
+        sDataFile = cfg["General"]["DataFile"]
+    except:
+        print("Error: Data file name not found in configuration file")
+        sys.exit(2)
+    try:
+        rEdgeLength = float(cfg["General"]["EdgeLength"])
+    except:
+        print("Error: Edge length not found or invalid in configuration file")
+        sys.exit(2)
