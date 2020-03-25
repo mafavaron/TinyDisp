f1b77b9adc0c8e3faf2c0ff1e052bdb174d11231
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-25 21:35:16 +0100

    Create meandering.py

---
+++ src/meandering.py
@@ -0,0 +1,12 @@
+import math
+
+if __name__ == "__main__":
+
+    # Constants
+    n = 721
+
+    # Generate a meandering wind, with constant standard deviations
+    f = open("meandering.csv", "w")
+    for i in range(n):
+
+    f.close()
