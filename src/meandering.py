410765bf4a7e73287b1299906de54a605686a046
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-26 16:32:33 +0100

    Update meandering.py
    Corrections

--- src/meandering.py
+++ src/meandering.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     f.write("Time.Stamp, U, V, sU, sV, cUV\n")
     for i in range(n):
         cur_time = time.gmtime(calendar.timegm(start_time) + i)
-        x = 6 * math.pi * i / n
+        x = 45 * math.cos(6 * math.pi * i / (n-1)) * math.pi / 180
         u = math.cos(x)
         v = math.sin(x)
         f.write("%s, %f, %f, 0.3, 0.3, 0.0\n" % (time.strftime("%Y-%m-%d %H:%M:%S", cur_time), u, v))
