82a596fca8dd8be9a1b3950b530420c46f853f5c
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-26 15:25:33 +0100

    Update meandering.py
    Correction

--- src/meandering.py
+++ src/meandering.py
@@ -9,12 +9,12 @@ if __name__ == "__main__":
 
     # Generate a meandering wind, with constant standard deviations
     start_time = time.strptime("2000-01-01 00:00:00", "%Y-%m-%d %H:%M:%S")
-    f = open(".\\meandering.csv", "w")
+    f = open(".\\meanderingwind.csv", "w")
     f.write("Time.Stamp, U, V, sU, sV, cUV\n")
     for i in range(n):
         cur_time = time.gmtime(calendar.timegm(start_time) + i)
         x = 6 * math.pi * i / n
         u = math.cos(x)
         v = math.sin(x)
-        f.write("%s, %f, %f, 0.3, 0.3, 0.0\n" % (cur_time, u, v))
+        f.write("%s, %f, %f, 0.3, 0.3, 0.0\n" % (time.strftime("%Y-%m-%d %H:%M:%S", cur_time), u, v))
     f.close()
