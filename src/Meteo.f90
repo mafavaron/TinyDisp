dbc2ea6db362dda441db2dfa4e9e4393bc0c3415
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-29 18:36:46 +0200

    Update Meteo.f90
    In progress

--- src/Meteo.f90
+++ src/Meteo.f90
@@ -1,10 +1,13 @@
 module Meteo
 
+	use calendar
+
 	implicit none
 	
 	private
 	
 	! Public interface
+	public	:: MeteoType
 	
 	! Data type
 	type MeteoType
@@ -39,6 +42,7 @@ contains
 		integer				:: iData
 		character(len=256)	:: sBuffer
 		integer				:: iYear, iMonth, iDay, iHour, iMinute, iSecond
+		logical				:: lSorted
 		
 		! Assume success (will falsify on failure)
 		iRetCode = 0
@@ -55,11 +59,17 @@ contains
 			if(iErrCode /= 0) exit
 			iNumData = iNumData + 1
 		end do
+		if(iNumData < 2) then
+			iRetCode = 2
+			close(iLUN)
+			return
+		end if
 		rewind(iLUN)
 		read(iLUN, "(a)") sBuffer	! Skip header (now, the "normal way"
 		do iData = 1, iNumData
 			read(iLUN, "(a)") sBuffer
 			read(sBuffer(1:19), "(i4,5()1x,i2)") iYear, iMonth, iDay, iHour, iMinute, iSecond
+			call PackTime(this % ivTimeStamp(iData), iYear, iMonth, iDay, iHour, iMinute, iSecond)
 			read(21:), *) &
 				this % rvU(iData), &
 				this % rvV(iData), &
@@ -70,70 +80,71 @@ contains
 		end do
 		close(iLUN)
 		
+		! Check data are sorted with respect to time
+		lSorted = .TRUE.
+		do iData = 2, iNumData
+			if(this % ivTimeStamp(iData-1) >= this % ivTimeStamp(iData)) then
+				lSorted = .FALSE.
+				exit
+			end if
+		end do
+		if(.not.lSorted) then
+			iRetCode = 3
+		end if
+		
 	end function met_read
 	
 	
-	function met_resample(this) result(iRetCode)
+	function met_resample(this, iTimeStep) result(iRetCode)
 
 		! Routine arguments
 		class(MeteoType), intent(out)	:: this
+		integer, intent(in)				:: iTimeStep
 		integer							:: iRetCode
 		
-			// Gather meteo data. By construction, meteo data are sorted
-			// ascending with respect to time stamps
-			std::string			sBuffer;
-			bool				lIsFirst = true;
-			std::vector<time_t>	ivTimeStamp;
-			std::vector<float>	rvU;
-			std::vector<float>	rvV;
-			std::vector<float>	rvStdDevU;
-			std::vector<float>	rvStdDevV;
-			std::vector<float>	rvCovUV;
-			while (std::getline(fMeteo, sBuffer)) {
-				if (lIsFirst) {
-					lIsFirst = false; // And, do nothing with the buffer - a header, in case
-				}
-				else {
-					static const std::string dateTimeFormat{ "%Y-%m-%d %H:%M:%S" };
-					std::vector<std::string> svFields;
-					split(sBuffer, svFields);
-					if (svFields.size() == 6) {
-						float rU       = stof(svFields[1]);
-						float rV       = stof(svFields[2]);
-						float rStdDevU = stof(svFields[3]);
-						float rStdDevV = stof(svFields[4]);
-						float rCovUV   = stof(svFields[5]);
-						if (rU > -9999.0f && rV > -9999.0f && rStdDevU > -9999.0f && rStdDevV > -9999.0f && rCovUV > -9999.0f) {
-							std::istringstream ss{svFields[0]};
-							struct tm tTimeStamp;
-							ss >> std::get_time(&tTimeStamp, dateTimeFormat.c_str());
-							ivTimeStamp.push_back(std::mktime(&tTimeStamp));
-							rvU.push_back(rU);
-							rvV.push_back(rV);
-							rvStdDevU.push_back(rStdDevU);
-							rvStdDevV.push_back(rStdDevV);
-							rvCovUV.push_back(rCovUV);
-						}
-					}
-				}
-			}
-
-			// Check some meteo data has been collected (if not, there is
-			// nothing to be made
-			if (ivTimeStamp.size() <= 0) return;
-
-			// Check configuration values
-			if (this->iTimeStep <= 0) return;
-			if (this->rEdgeLength <= 0.0f) return;
-			if (this->iPartsPerStep <= 0) return;
-			if (this->iStepsSurvival <= 0) return;
-
-			// Interpolate linearly in the time range of meteo data
+		! Locals
+		integer								:: iNumMeteoData
+		integer, dimension(:), allocatable	:: ivTimeStamp
+		real, dimension(:), allocatable		:: rvU
+		real, dimension(:), allocatable		:: rvV
+		real, dimension(:), allocatable		:: rvW
+		real, dimension(:), allocatable		:: rvStdDevU
+		real, dimension(:), allocatable		:: rvStdDevV
+		real, dimension(:), allocatable		:: rvStdDevW
+		real, dimension(:), allocatable		:: rvCovUV
+		real, dimension(:), allocatable		:: rvCovUW
+		real, dimension(:), allocatable		:: rvCovVW
+		integer								:: iIdx
+		integer								:: iTimeStamp
+		integer								:: iLastTime
+		integer								:: iNumElements
+		
+		! Assume success (will falsify on failure)
+		iRetCode = 0
+		
+		! Reserve workspace
+		iIdx         = 1
+		iTimeStamp   = this % ivTimeStamp(iIdx)
+		iLastTime    = this % ivTimeStamp(size(this % ivTimeStamp))
+		iNumElements = (iLastTime - iTimeStamp) / iTimeStep
+		if(iNumElements <= 1) then
+			iRetCode = 1
+			return
+		end if
+		allocate(ivTimeStamp(iNumElements))
+		allocate(rvU(iNumElements))
+		allocate(rvV(iNumElements))
+		allocate(rvW(iNumElements))
+		allocate(rvStdDevU(iNumElements))
+		allocate(rvStdDevV(iNumElements))
+		allocate(rvStdDevW(iNumElements))
+		allocate(rvCovUV(iNumElements))
+		allocate(rvCovUW(iNumElements))
+		allocate(rvCovVW(iNumElements))
+		
+		! Main loop: locate sampling time stamps, and linearly interpolate the original data
+		! at them
 			std::vector<float> rvInterpDeltaTime;
-			int				   iIdx = 0;
-			time_t             iTimeStamp = ivTimeStamp[iIdx];
-			time_t             iLastTime = ivTimeStamp[ivTimeStamp.size() - 1];
-			int                iNumElements = (iLastTime - iTimeStamp) / this->iTimeStep;
 			this->ivTimeStamp.reserve(iNumElements);
 			this->rvU.reserve(iNumElements);
 			this->rvV.reserve(iNumElements);
@@ -189,7 +200,19 @@ contains
 				}
 
 				iTimeStamp += this->iTimeStep;
+				
+		! Leave
+		deallocate(rvCovVW)
+		deallocate(rvCovUW)
+		deallocate(rvCovUV)
+		deallocate(rvStdDevW)
+		deallocate(rvStdDevV)
+		deallocate(rvStdDevU)
+		deallocate(rvW)
+		deallocate(rvV)
+		deallocate(rvU)
+		deallocate(ivTimeStamp)
 
-			}
+	end function met_resample
 
 end module Meteo
