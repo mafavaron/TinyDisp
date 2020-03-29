7be7df6f5e8f0bfebe8bb5c8ea45fc3d39e0aac1
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-29 21:00:46 +0200

    Update Meteo.f90
    Heading comments added

--- src/Meteo.f90
+++ src/Meteo.f90
@@ -1,3 +1,11 @@
+! Meteo - Fortran module for reading TinyDisp specific meteorological information, and
+! refining it by linear interpolation.
+!
+! Copyright 2020 by Servizi Territorio srl
+!                   This is open-source software, covered by the MIT license.
+!
+! Author: Patti M. Favaron
+!
 module Meteo
 
 	use calendar
