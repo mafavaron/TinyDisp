3913000513f947babf17dcc4b02c9b08481f4330
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-30 16:04:05 +0200

    Update NormalDeviates.f90
    Adding multivariate normal

--- src/NormalDeviates.f90
+++ src/NormalDeviates.f90
@@ -13,19 +13,21 @@ module NormalDeviates
 	private
 	
 	! Public interface
-	public	:: rNorm
+	public	:: Norm
 
 contains
 
 	! Credit: this routine has been obtained by refactoring of function "random_normal"
 	! by Alan Miller (CSIRO Division of Mathematics & Statistics), module 'random.f90'
-	elemental function rNorm() result(rNormValue)
+	function Norm(rvNormValues) result(iRetCode)
 	
 		! Routine arguments
-		real	:: rNormValue
+		real, dimension(:), intent(inout)	:: rvNormValues
+		integer								:: iRetCode
 
 		! Locals
 		real	:: u, v, x, y, q
+		integer	:: i
 		
 		! Constants
 		real, parameter	:: s  =  0.449871
@@ -34,31 +36,89 @@ contains
 		real, parameter	:: b  =  0.25472
 		real, parameter	:: r1 =  0.27597
 		real, parameter	:: r2 =  0.27846
-
-		! Main loop: generate P = (u,v) uniform in rectangle enclosing
-		! acceptance region, and iterate until point is really within
-		! the acceptance region
-		do
 		
-			! Get uniform deviates using the compiler's generator
-			call RANDOM_NUMBER(u)
-			call RANDOM_NUMBER(v)
-			v = 1.7156 * (v - 0.5)
+		! Assume success (will falsify on failure)
+		iRetCode = 0
 
-			! Evaluate the quadratic form
-			x = u - s
-			y = ABS(v) - t
-			q = x**2 + y*(a*y - b*x)
+		! Main loop
+		if(size(rvNormValues) <= 0) then
+			iRetCode = 1
+			return
+		end if
+		do i = 1, size(rvNormValues)
 
-			! Accept/reject
-			if(q < r1) exit
-			if(q > r2) cycle
-			if(v**2 < -4.0*LOG(u)*u**2) exit
+			! Important loop: generate P = (u,v) uniform in rectangle enclosing
+			! acceptance region, and iterate until point is really within
+			! the acceptance region
+			do
+			
+				! Get uniform deviates using the compiler's generator
+				call RANDOM_NUMBER(u)
+				call RANDOM_NUMBER(v)
+				v = 1.7156 * (v - 0.5)
+
+				! Evaluate the quadratic form
+				x = u - s
+				y = ABS(v) - t
+				q = x**2 + y*(a*y - b*x)
+
+				! Accept/reject
+				if(q < r1) exit
+				if(q > r2) cycle
+				if(v**2 < -4.0*LOG(u)*u**2) exit
+			end do
+
+			! Return ratio of P's coordinates as the normal deviate
+			rvNormValues(i) = v/u
+		
 		end do
 
-		! Return ratio of P's coordinates as the normal deviate
-		rNormValue = v/u
+	end function Norm
+	
+	
+	! This is all mine (Patti)
+	function MultiNorm(rU, rV, rW, rUU, rVV, rWW, rUV, rUW, rVW, rvNormU, rvNormV, rvNormW) result(iRetCode)
+	
+		! Routine arguments
+		real, intent(in)				:: rU
+		real, intent(in)				:: rV
+		real, intent(in)				:: rW
+		real, intent(in)				:: rUU
+		real, intent(in)				:: rVV
+		real, intent(in)				:: rWW
+		real, intent(in)				:: rUV
+		real, intent(in)				:: rUW
+		real, intent(in)				:: rVW
+		real, dimension(:), intent(out)	:: rvNormU
+		real, dimension(:), intent(out)	:: rvNormV
+		real, dimension(:), intent(out)	:: rvNormW
+		integer							:: iRetCode
+		
+		! Locals
+		
 
-	end function rNorm
+		! Compute the coefficients of the Cholesky decomposition of covariances matrix
+		L11 = SQRT(rUU)
+		L12 = rUV/L11
+		L13 = rUW/L11
+		L22 = SQRT(-(rUV**2/rUU) + rVV)
+		L23 = (-rUV*rUW + rUU*rVW) / &
+				SQRT(rUU*(-rUV**2 + rUU*rVV))
+		L33 = SQRT(-(rUW**2/rUU) - &
+				(-rUV*rUW + rUU*rVW) * &
+				(-rUV*rUW + rUU*rVW) / &
+				(rUU**2*(-(rUV**2/rUU) + rVV)) + rWW)
+		
+		! Update particles positions
+		DO i = 1, iNumPart
+		
+			! Generate three random values distributed according to the multivariate
+			! normal distribution with same covariance matrix as computed
+			rRandX = random_Normal()
+			rRandY = random_Normal()
+			rRandZ = random_Normal()
+			rRandU = L11*rRandX + L12*rRandY + L13*rRandZ
+			rRandV =              L22*rRandY + L23*rRandZ
+			rRandW =                           L33*rRandZ
 
 end module NormalDeviates
