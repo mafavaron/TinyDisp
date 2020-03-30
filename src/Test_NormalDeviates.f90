7c4570d3c82e3126691eefd355eb88b660e2bb52
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-30 18:25:38 +0200

    Update Test_NormalDeviates.f90
    Test 1: Univariate generation

--- src/Test_NormalDeviates.f90
+++ src/Test_NormalDeviates.f90
@@ -6,4 +6,33 @@
 ! Author: Patti M. Favaron
 !
 program Test_NormalDeviates
+
+	use NormalDeviates
+	
+	implicit none
+	
+	! Locals
+	integer					:: iRetCode
+	integer					:: i
+	real, dimension(100000)	:: rvNormU
+	real, dimension(100000)	:: rvNormV
+	real, dimension(100000)	:: rvNormW
+	real, parameter			:: rU  =  0.
+	real, parameter			:: rV  =  1.
+	real, parameter			:: rW  =  4.
+	real, parameter			:: rUU =  0.25
+	real, parameter			:: rVV =  0.25
+	real, parameter			:: rWW =  0.25
+	real, parameter			:: rUV = -0.1
+	real, parameter			:: rUW =  0.1
+	real, parameter			:: rVW =  0.05
+	
+	! Generate a univariate sample N(0,1)
+	iRetCode = Norm(rvNormU)
+	open(10, file='Norm_Uni.csv', status='unknown', action='write')
+	do i = 1, size(rvNormU)
+		write(10, "(e15.7)") rvNormU(i)
+	end do
+	close(10)
+
 end program Test_NormalDeviates
