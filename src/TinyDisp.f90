b3b456f033f186b71f537e929ef5aa73bac76c4a
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-30 21:41:17 +0200

    Update TinyDisp.f90
    Structure defined, more or less.

--- src/TinyDisp.f90
+++ src/TinyDisp.f90
@@ -10,5 +10,23 @@ program TinyDisp
 	use Meteo
 	
 	implicit none
+	
+	! Locals
+	
+	! Get input parameters
+	
+	! Get configuration
+	
+	! Read meteo data, and expand it to the desired time step
+	
+	! Initialize particles count
+	
+	! Main loop: iterate over all time steps, and simulate transport and diffusion
+	
+		! Emit new particles
+		
+		! Move particles
+		
+		! Save active particles
 
 end program TinyDisp
