4703d7961fb83785ae14d7d25fc1a19666f46fa1
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-30 18:17:22 +0200

    Create Test_NormalDeviates.f90

---
+++ src/Test_NormalDeviates.f90
@@ -0,0 +1,9 @@
+! Test driver for module "NormalDeviates.f90".
+!
+! Copyright 2020 by Servizi Territorio srl
+!                   This is open-source software, covered by the MIT license.
+!
+! Author: Patti M. Favaron
+!
+program Test_NormalDeviates
+end program Test_NormalDeviates
