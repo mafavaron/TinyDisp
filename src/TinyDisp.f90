22c472b67013502f1cf4ce11f681953c6c1eb8e5
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-30 20:59:55 +0200

    Create TinyDisp.f90

---
+++ src/TinyDisp.f90
@@ -0,0 +1,14 @@
+! Test driver for module "NormalDeviates.f90".
+!
+! Copyright 2020 by Servizi Territorio srl
+!                   This is open-source software, covered by the MIT license.
+!
+! Author: Patti M. Favaron
+!
+program TinyDisp
+
+	use Meteo
+	
+	implicit none
+
+end program TinyDisp
