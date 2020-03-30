9435056eb4da7449a6eec406271882cb500586b9
Patrizia M. Favaron <mafavaron@mac.com>
2020-03-30 14:56:11 +0200

    Create NormalDeviates.f90

---
+++ src/NormalDeviates.f90
@@ -0,0 +1,11 @@
+! NormalDeviates - Fortran module for generating univariate and three-variate
+!                  random deviates.
+!
+! Copyright 2020 by Servizi Territorio srl
+!                   This is open-source software, covered by the MIT license.
+!
+! Author: Patti M. Favaron
+!
+module NormalDeviates
+
+end module NormalDeviates
