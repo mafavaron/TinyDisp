6ba55b3f0eda4bb03e35e7481b52f8999c278ed5
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-02 23:48:08 +0200

    Update TinyDisp.f90
    OMP references added

--- src/TinyDisp.f90
+++ src/TinyDisp.f90
@@ -7,11 +7,13 @@
 !
 program TinyDisp
 
+    use omp_lib
 	use Meteo
 	
-	implicit none
+    implicit none
 	
 	! Locals
+    integer:: thread_id, nthreads
 	
 	! Get input parameters
 	
@@ -22,6 +24,19 @@ program TinyDisp
 	! Initialize particles count
 	
 	! Main loop: iterate over all time steps, and simulate transport and diffusion
+
+    !$omp parallel private(thread_id)
+
+    thread_id = omp_get_thread_num()
+    write (*,*) 'Hello World from thread', thread_id
+
+    !$omp barrier
+    if ( thread_id == 0 ) then
+        nthreads = omp_get_num_threads()
+        write (*,*) 'There are', nthreads, 'threads'
+    end if
+    
+    !$omp end parallel
 	
 		! Emit new particles
 		
