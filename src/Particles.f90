f6ecaa18e3e19bb0b8b816188fba738cbbb7426c
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-03 06:38:38 +0200

    Create Particles.f90

---
+++ src/Particles.f90
@@ -0,0 +1,10 @@
+! Particles - Fortran module for particles generation and dynamics.
+!
+! Copyright 2020 by Servizi Territorio srl
+!                   This is open-source software, covered by the MIT license.
+!
+! Author: Patti M. Favaron
+!
+module Particles
+
+end module Particles
