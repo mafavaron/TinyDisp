ccf829bf137b7bcc2f100665fde1bfd6f696eea3
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-13 09:41:35 +0200

    Create dia_part.f90

---
+++ dia_part/dia_part.f90
@@ -0,0 +1,9 @@
+! Main program of dia_part
+!
+! Copyright 2020 by Servizi Territorio srl
+!                   This is open-source software, covered by the MIT license.
+!
+! Author: Patti M. Favaron
+!
+program dia_part
+end program dia_part
