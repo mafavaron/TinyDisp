2a94ea9a3de58e6c91ef3fa2d72d9d24de3f9524
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-15 06:06:02 +0200

    Update met_split.f90
    In progress

--- met_split/met_split.f90
+++ met_split/met_split.f90
@@ -31,5 +31,22 @@ program met_split
     integer, dimension(:), allocatable  :: ivDayEnd
     integer                             :: iNumDays
     character(len=256)                  :: sBuffer
+    
+    ! Get command arguments
+    if(command_argument_count() /= 3) then
+        print *, 'met_split - Program to analyze and day-split a TinyDisp meteo file'
+        print *
+        print *, 'Usage:'
+        print *
+        print *, '  met_split <Met file> <Dia file> <Output prefix>'
+        print *
+        print *, 'Copyright 2020 by Servizi Territorio srl'
+        print *, '                  This is open-source software, covered by the MIT license.'
+        print *
+        stop
+    end if
+    call get_command_argument(1, sInputFile)
+    call get_command_argument(2, sDiaFile)
+    call get_command_argument(3, sOutputPrefix)
 
 end program met_split
