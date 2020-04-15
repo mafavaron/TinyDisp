0f7034f6a063aa1affd034c584dfab8052967313
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-15 03:02:02 +0200

    Update met_split.f90
    In progress

--- met_split/met_split.f90
+++ met_split/met_split.f90
@@ -8,6 +8,28 @@
 !
 program met_split
 
-
+    use calendar
+    
+    implicit none
+    
+    ! Local variables
+    integer                             :: iRetCode
+    character(len=256)                  :: sInputFile
+    character(len=256)                  :: sDiaFile
+    character(len=256)                  :: sOutputPrefix
+    integer, dimension(:), allocatable  :: ivTimeStamp
+    real, dimension(:), allocatable     :: rvU
+    real, dimension(:), allocatable     :: rvV
+    real, dimension(:), allocatable     :: rvW
+    real, dimension(:), allocatable     :: rvStdDevU
+    real, dimension(:), allocatable     :: rvStdDevV
+    real, dimension(:), allocatable     :: rvStdDevW
+    real, dimension(:), allocatable     :: rvCovUV
+    real, dimension(:), allocatable     :: rvCovUW
+    real, dimension(:), allocatable     :: rvCovVW
+    integer, dimension(:), allocatable  :: ivDayBegin
+    integer, dimension(:), allocatable  :: ivDayEnd
+    integer                             :: iNumDays
+    character(len=256)                  :: sBuffer
 
 end program met_split
