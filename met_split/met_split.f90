64d2ff25639be4c291eb5b89fbb223ed2bd66c71
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-15 02:52:47 +0200

    Create met_split.f90

---
+++ met_split/met_split.f90
@@ -0,0 +1,13 @@
+! Main program of met_split, the analyzer and day-splitter of
+! meteorological data for TinyDisp.
+!
+! Copyright 2020 by Servizi Territorio srl
+!                   This is open-source software, covered by the MIT license.
+!
+! Author: Patti M. Favaron
+!
+program met_split
+
+
+
+end program met_split
