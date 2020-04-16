a423a76dcce2a8e0285040c3658f967c5871bb49
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-16 18:51:54 +0200

    Create artificial.f90

---
+++ artificial/artificial.f90
@@ -0,0 +1,16 @@
+! artificial - Artificial case generator for TinyDisp.
+!
+! Copyright 2020 by Servizi Territorio srl
+!                   This is open-source software, covered by the MIT license.
+!
+! Author: Patti M. Favaron
+!
+program artificial
+
+    use Calendar
+    
+    implicit none
+    
+    ! Locals
+    
+end program artificial
