d56fef0a59856b5b444ceed6ba8aa853e8770ccb
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-16 10:55:08 +0200

    Update counter.f90
    In progress

--- counter/counter.f90
+++ counter/counter.f90
@@ -1,5 +1,4 @@
-! Meteo - Fortran module for reading TinyDisp specific meteorological information, and
-! refining it by linear interpolation.
+! counter - Program to summarize gridded particles counts from TinyDisp.
 !
 ! Copyright 2020 by Servizi Territorio srl
 !                   This is open-source software, covered by the MIT license.
@@ -7,5 +6,32 @@
 ! Author: Patti M. Favaron
 !
 program counter
+
+    use Calendar
+    
+    implicit none
+    
+    ! Locals
+    character(len=256)                      :: sInputFile
+    character(len=256)                      :: sOutputFile
+    integer, dimension(:,:), allocatable    :: imCount
+    integer, dimension(:,:), allocatable    :: imTotal
+    integer                                 :: iYear, iMonth, iDay, iHour, iMinute, iSecond
+    
+    ! Get parameters
+    if(command_argument_count() /= 2) then
+        print *, "counter - Program to summarize gridded particles counts from TinyDisp"
+        print *
+        print *, "Usage:"
+        print *
+        print *, "  counter <Gridded_Count_File> <Output_File>"
+        print *
+        print *, "Copyright 2020 by Servizi Territorio srl"
+        print *, "                  This is open-source software, covered by the MIT license"
+        print *
+        stop
+    end if
+    call getarg(1, sInputFile)
+    call getarg(2, sOutputFile)
 
 end program counter
