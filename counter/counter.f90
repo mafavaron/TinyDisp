58c3febd002542940ff0f7ece1d68253790523b6
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-16 11:33:18 +0200

    Update counter.f90
    In progress: first access to file done

--- counter/counter.f90
+++ counter/counter.f90
@@ -12,11 +12,16 @@ program counter
     implicit none
     
     ! Locals
+    integer                                 :: iRetCode
     character(len=256)                      :: sInputFile
     character(len=256)                      :: sOutputFile
     integer, dimension(:,:), allocatable    :: imCount
     integer, dimension(:,:), allocatable    :: imTotal
     integer                                 :: iYear, iMonth, iDay, iHour, iMinute, iSecond
+    real                                    :: rXmin
+    real                                    :: rYmin
+    real                                    :: rDxy
+    integer                                 :: iNumCells
     
     ! Get parameters
     if(command_argument_count() /= 2) then
@@ -33,5 +38,19 @@ program counter
     end if
     call getarg(1, sInputFile)
     call getarg(2, sOutputFile)
+    
+    ! Access the count file
+    open(10, file=sInputFile, action="read", status="old", access='stream', iostat=iRetCode)
+    if(iRetCode /= 0) then
+        print *, 'counter:: error: Input file not opened'
+        stop
+    end if
+    read(10, iostat=iRetCode) rXmin, rYmin, rDxy, iNumCells
+    if(iRetCode /= 0) then
+        print *, 'counter:: error: Input file is invalid'
+        stop
+    end if
+    
+    close(10)
 
 end program counter
