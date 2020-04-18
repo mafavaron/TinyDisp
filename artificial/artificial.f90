bdce98837e4eb8c05852a2f5c74707f17e4d90dc
Patrizia M. Favaron <mafavaron@mac.com>
2020-04-18 10:11:51 +0200

    Update artificial.f90
    Circular case added

--- artificial/artificial.f90
+++ artificial/artificial.f90
@@ -50,6 +50,23 @@ program artificial
     
     elseif(iOptCode == 2) then
     
+        open(10, file=sOutputFile, status='unknown', action='write')
+        write(10, "('Time.Stamp, U, V, W, StdDev.U, StdDev.V, StdDev.W, Cov.UV, Cov.UW, Cov.VW')")
+        do iFrame = 1, iNumFrames
+            iCurTime = iStartTime + (iFrame - 1) * iTimeStep
+            call unpacktime(iCurTime, iYear, iMonth, iDay, iHour, iMinute, iSecond)
+            write(10, "(i4.4,2('-',i2.2),1x,i2.2,2(':',i2.2),3(',',f8.2),6(',',f8.4))") &
+                iYear, iMonth, iDay, iHour, iMinute, iSecond, &
+                rVel * cos(-TO_RAD * (rDir + 720.*iNumLoops*(iFrame-1)/float(iNumFrames-1))), &
+                rVel * sin(-TO_RAD * (rDir + 720.*iNumLoops*(iFrame-1)/float(iNumFrames-1))), &
+                0.0, &
+                rSigma, rSigma, &
+                0.0, &
+                rCov, &
+                0.0, 0.0
+        end do
+        close(10)
+    
     else
     
         print *, "artificial:: error: Unsupported command"
